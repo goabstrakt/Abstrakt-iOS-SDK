6f077830e2348bc37e66ef015b48f90a8e75562b
macintosh <ameed.ansari@creatrixe.com>
2019-06-24 13:52:14 -0700

    First push with README and Framework

---
+++ AbstraktSDK.framework/Headers/AbstraktSDK.h
@@ -0,0 +1,19 @@
+//
+//  AbstraktSDK.h
+//  AbstraktSDK
+//
+//  Created by macintosh on 11/06/2019.
+//  Copyright © 2019 Vault Wallet LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AbstraktSDK.
+FOUNDATION_EXPORT double AbstraktSDKVersionNumber;
+
+//! Project version string for AbstraktSDK.
+FOUNDATION_EXPORT const unsigned char AbstraktSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AbstraktSDK/PublicHeader.h>
+
+
